be7ffedcb9f97875c570ad61962eafda9e1fe19f
inesjahir <167090620+inesjahir@users.noreply.github.com>
2024-04-19 14:27:52 +0200

    Add files via upload

---
+++ projet vitamin D analyser.py
@@ -0,0 +1,52 @@
+ # projet vitamin D analyser .
+print ("bonjour , bienvenue dans ce 'vitamin analyser' veuillez introduire les donnees suivantes :  ")
+quantitedesang =(float(input("quel est quantite de sang de cet echantillon?")))
+quantitevitaminD=(float(input("quel quantite de vitamin D a ete trouve danscet echantillon ?")))
+
+concentration =(float(quantitevitaminD/quantitedesang))
+
+
+print ("la concentration est de :" +str (concentration) + " ng/ml") 
+
+
+if  concentration <=10 :
+   print ("diagnostic ""=>"" ""en"" ""deficience")
+   
+if  concentration <=30 or concentration >10 :
+    print ("diagnostic ""=>"" ""en"" ""insuffisance")
+    
+if concentration <100 or concentration >30 :
+    print ("diagnostic ""=>"" ""en"" ""suffisance")
+    
+else:
+    print ("diagnostic ""=>"" ""en"" ""exces")
+    
+    
+    
+   
+   
+   
+   
+
+
+
+
+
+
+'''    
+if  concentration <30:
+    print ("diagnostic ""=>"" ""en"" ""insuffisance")
+else :
+    print ("diagnostic ""=>"" ""en"" ""suffisance")
+
+if concentration >30:
+    print ("diagnostic ""=>"" ""en"" ""suffisance")
+else:
+    print ("diagnostic ""=>"" ""en"" ""suffisance")   
+        
+if  concentration <100:
+    print ("diagnostic ""=>"" ""en"" ""suffisance")
+'''
+        
+    
+    
